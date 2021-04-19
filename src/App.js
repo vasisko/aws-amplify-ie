2910785b76a66623240a9feee16b96a7adea034a
Carolyn Vasisko <cvasisko@carolyns-air.attlocal.net>
2021-04-19 15:27:19 -0500

    add amplify with auth to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -7,12 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the Amplify App Ex
+          Welcome to the Amplify App Ex:<br />
+          We now have Auth!
         </p>
-        
+        <AmplifySignOut />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
